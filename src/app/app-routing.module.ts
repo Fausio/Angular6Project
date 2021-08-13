19e755845f87135a82b3c11eff990d23ad34b591
Fáusio <luisfausio@gmail.com>
2021-08-13 14:59:05 +0200

    angular route and app.component.html nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+import { ListEmployeesComponent } from './employee/list-employees.component';
 
 const routes: Routes = [
   
-
+  { path: 'create', component: CreateEmployeeComponent } ,
+  { path: 'list', component: ListEmployeesComponent } ,
   // ---------------------------Wildcard as a Catch-All Route-----------------------------------
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '/list' }
 
 ];
 
@@ -13,4 +16,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
