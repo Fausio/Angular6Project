21adb0c5e46a2e824d4b33377387df7f43181dd9
Fáusio <luisfausio@gmail.com>
2021-08-13 10:56:02 +0200

    Wildcard as a Catch-All Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+
+  // ---------------------------Wildcard as a Catch-All Route-----------------------------------
+  { path: '**', redirectTo: '' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
