2b857de337769cf430facdf8e081869c32ba7ec2
fausio luis <fausioluis@live.com>
2021-08-15 12:06:39 +0200

    EmpployeeForm

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -9,7 +10,16 @@ export class CreateEmployeeComponent implements OnInit {
 
   constructor() { }
 
+  EmpployeeForm : FormGroup| any;
+
   ngOnInit(): void {
+
+    this.EmpployeeForm = new FormGroup({
+
+      FullName: new FormControl(),
+      Gender: new FormControl
+
+    })
   }
 
 }
